8fe81beedcf7d2c95d575a52dc31ccc61511ba8f
Maikel P. C. Finck <maikel@ciandt.com>
2020-04-08 15:31:11 -0300

    Install composer

---
+++ install-composer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Refresh the package index.
+sudo apt update
+
+# Install requirements.
+sudo apt install wget php-cli php-zip unzip
+
+# Install composer.
+curl -sS https://getcomposer.org/installer | sudo php && sudo mv composer.phar /usr/local/bin/composer
+
+composer --version
