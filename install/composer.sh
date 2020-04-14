c20112b50fe04a3f0aed1e41ea9a69d084844048
Maikel P. C. Finck <maikel@ciandt.com>
2020-04-14 14:15:29 -0300

    Fix permissions and dependencies.

--- install/composer.sh
+++ install/composer.sh
@@ -4,9 +4,13 @@
 sudo apt update
 
 # Install requirements.
-sudo apt install --yes wget php-cli php-zip unzip
+sudo apt install --yes wget php-cli php-zip php-xml unzip
 
 # Install composer.
 curl -sS https://getcomposer.org/installer | sudo php && sudo mv composer.phar /usr/local/bin/composer
+
+# Fix permissions.
+sudo chmod +x /usr/local/bin/composer
+sudo chown -R $USER ~/.composer/
 
 composer --version
