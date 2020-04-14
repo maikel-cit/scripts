2e92f63b9e1daef683d8e9c5e71f74082f556ebe
Maikel P. C. Finck <maikel@ciandt.com>
2020-04-14 14:15:42 -0300

    Complete script commands.

--- install/drush.sh
+++ install/drush.sh
@@ -5,6 +5,14 @@ NC=$(tput sgr0)
 # Colors
 YELLOW=$(tput setaf 3)
 
+# Install Drush commandline tool.
 composer global require drush/drush
 
+# Create symbolic link to execute it.
 sudo ln -s ~/.composer/vendor/drush/drush/drush /usr/local/bin/drush
+
+# Fix permissions
+sudo chmod +x /usr/local/bin/drush
+
+echo
+drush --version
