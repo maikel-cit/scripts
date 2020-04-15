abfd8a706fb7a37b1fa2b809975c935c43b2eeef
Maikel P. C. Finck <maikel@ciandt.com>
2020-04-15 09:45:00 -0300

    Add the custom theme to zshrc.

--- install/zsh.sh
+++ install/zsh.sh
@@ -4,10 +4,16 @@
 sudo apt install --yes zsh
 
 # Install Oh-My-Zsh.
-git clone https://github.com/ohmyzsh/ohmyzsh.git ~/.oh-my-zsh
+git clone https://github.com/ohmyzsh/ohmyzsh.git $HOME/.oh-my-zsh
 
 # Set the default RC file.
-cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+
+# Download the custom theme.
+wget https://raw.githubusercontent.com/maikel-cit/scripts/master/.oh-my-zsh/themes/maikel-cit.zsh-theme -O $HOME/.oh-my-zsh/themes/maikel-cit.zsh-theme
+
+# Replace the default theme.
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="maikel-cit"/g' $HOME/.zshrc
 
 # Enable the ZSH terminarl.
 chsh -s $(which zsh)
