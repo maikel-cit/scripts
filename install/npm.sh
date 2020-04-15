f60ee3a36af3d0a7a9c9fe6e271c0a0cd2d52510
Maikel P. C. Finck <maikel@ciandt.com>
2020-04-15 11:05:03 -0300

    Installing NPM.

---
+++ install/npm.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# No Color
+NC=$(tput sgr0)
+# Colors
+YELLOW=$(tput setaf 3)
+
+# Download the PPA setup.
+curl -sL https://deb.nodesource.com/setup_10.x -o $HOME/Downloads/nodesource_setup.sh
+
+# Install the PPA.
+sudo bash $HOME/Downloads/nodesource_setup.sh
+
+# Remove the installation file.
+rm $HOME/Downloads/nodesource_setup.sh
+
+# Install NODEJS.
+sudo apt install --yes nodejs
+
+echo
+echo "${YELLOW}NodeJS: "
+nodejs -v
+
+echo
+echo "NPM: "
+npm -v
+
+echo "${NC}"
